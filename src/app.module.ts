c7d794c47e9c9b5757525490ab57623b59631507
Anghel <amelanie.trillo27@gmail.com>
2022-11-22 14:02:42 -0500

    Add swagger settings

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { ClientsModule } from './clients/clients.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      url: 'mysql://root:root@localhost:3306/banking-ddd-scratch',
+      migrationsRun: true,
+      logging: true,
+      timezone: '+00:00',
+      bigNumberStrings: false,
+      entities: ['dist/**/infrastructure/persistence/entities/*{.ts,.js}'],
+      subscribers: [],
+      migrations: [
+        'dist/shared/infrastructure/persistence/migrations/*{.ts,.js}',
+      ],
+      migrationsTableName: 'migrations',
+    }),
+    ClientsModule,
+  ],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule {}
